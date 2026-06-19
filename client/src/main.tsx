054ef5edd8523feaebf1395632e404fd9184af39
Smith Patil <smith@example.com>
2026-06-19 14:44:39 +0530

    Configure frontend to use live Render API URL

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'https://smart-attendance-api-0via.onrender.com'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
